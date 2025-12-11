ff66f69833797428d84347522029ce322d039cb0
vorozhkog <vorozhkog@icloud.com>
2025-12-11 16:46:44 +0000

    get data dir from method

--- src/sly_globals.py
+++ src/sly_globals.py
@@ -29,7 +29,7 @@ if INPUT_DIR:
 else:
     IS_ON_AGENT = api.file.is_on_agent(INPUT_FILE)
 
-STORAGE_DIR: str = "/app/data/"
+STORAGE_DIR = sly.app.get_data_dir()
 mkdir(STORAGE_DIR, True)
 
 ANN_EXT = ".json"
